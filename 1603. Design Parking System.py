eb2637459198694c1c69395583978345a88c01e7
Daniel Lartey <daniel.lartey@outlook.com>
2023-10-04 13:52:26 +0100

    neetcode

---
+++ 1603. Design Parking System.py
@@ -0,0 +1,21 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+            self.table = collections.defaultdict(int)
+            self.table[1] = big
+            self.table[2] = medium
+            self.table[3] = small
+        
+
+    def addCar(self, carType: int) -> bool:
+        if self.table[carType] > 0:
+            self.table[carType]-=1
+            return True
+        else:
+            return False
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
